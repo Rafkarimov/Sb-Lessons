219bfad460aca61d6f499d1ecaef6487d5c1002d
Rafail <raf_karimov@yahoo.com>
2022-12-26 00:09:00 +0300

    v. hw 4.1.6\1 ДЗ ДОП

--- src/main/homedop/dop/MainMap.java
+++ src/main/homedop/dop/MainMap.java
@@ -14,6 +14,7 @@ package main.homedop.dop;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
@@ -21,37 +22,49 @@ import java.util.stream.Stream;
 public class MainMap {
     public static void main(String[] args) {
         Map<String, Integer> wordsStartingWithLettersMap = new HashMap<>();
-//        Map<Integer, Integer> wordsByLengthMap = new HashMap<>();
+        Map<Integer, Integer> wordsByLengthMap = new HashMap<>();
 
-        List<String> list = new ArrayList<>();
-        list.add("даба");
-        list.add("даба");
-        list.add("ворожба");
-        list.add("разведслужба");
-        list.add("дамба");
-        list.add("даба");
-        list.add("авиабомба");
-        list.add("даба");
-
-        for (String item : list) {
-            String meaning = Character.toString(item.charAt(0));
-            if (wordsStartingWithLettersMap.containsKey(meaning.substring(0, 1))) {
-                wordsStartingWithLettersMap.put(meaning, wordsStartingWithLettersMap.get(meaning) + 1);
-            } else {
-                wordsStartingWithLettersMap.put(meaning, 1);
-            }
-        }
-        System.out.println(wordsStartingWithLettersMap);
-
-
-//        try (Stream<String> stream = Files.lines(Paths.get("C:\\Users\\User\\IdeaProjects\\Sb-Lessons\\src\\main" +
-//                "\\homedop\\dop\\dictionary.txt"))) {
-//            stream.forEach((String word) -> {
+//        List<String> list = new ArrayList<>();
+//        list.add("даба");
+//        list.add("даба");
+//        list.add("ворожба");
+//        list.add("разведслужба");
+//        list.add("дамба");
+//        list.add("даба");
+//        list.add("авиабомба");
+//        list.add("даба");
 //
-//            });
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
+//        for (String item : list) {
+//            String meaning = Character.toString(item.charAt(0));
+//            if (wordsStartingWithLettersMap.containsKey(meaning.substring(0, 1))) {
+//                wordsStartingWithLettersMap.put(meaning, wordsStartingWithLettersMap.get(meaning) + 1);
+//            } else {
+//                wordsStartingWithLettersMap.put(meaning, 1);
+//            }
 //        }
+//        System.out.println(wordsStartingWithLettersMap);
+//
+//        for (String item : list) {
+//
+//        }
+
+        //Класс Files содержит статические методы для работы с файлами и каталогами. Полезный метод - lines() который
+        // возвращает поток строк: Stream<String> . Из этого потока можно получить строки, содержащиеся в файле.
+        //Метод принимает Path к файлу, который мы хотели бы прочитать, с необязательной Charset . Мы будем использовать
+        // try-with-resources для автоматизации очистки и закрытия:
+        Path path = Paths.get("dictionary.txt");
+
+        //Поскольку метод возвращает Stream , мы используем его forEach() для перебора строк со ссылкой на метод для краткости.
+        //Вместо печати каждой линии, StringBuilder может быть использован для добавления строк:
+        StringBuilder sb = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(path)) {
+            stream.forEach((String word) -> {
+
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
